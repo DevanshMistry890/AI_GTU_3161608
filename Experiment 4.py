96ec30cd929c7a673b11bd13dbe5519bed62ca31
Devansh Mistry <73028874+DevanshMistry890@users.noreply.github.com>
2022-03-18 14:04:26 +0530

    Experiment 4

---
+++ Experiment 4.py
@@ -0,0 +1,22 @@
+#python for educative guess
+#code by Devansh Mistry
+import random
+print("| GUESS THE NUMBER |")
+print('\n')
+print('Range of Random Numbers.')
+start = int(input('Enter Starting Index: '))
+end = int(input('Enter Ending Index: '))
+number = random.randint(start, end)
+print('\n')
+while True:
+    guess = int(input('Guess the number:  '))
+    if guess > number:
+        print('\nHmmm, try a lower number...\n')
+
+    elif guess < number:
+        print('\nGo a little higher\n')
+
+    else:
+        print("Right on! Well done!")
+        break
+
